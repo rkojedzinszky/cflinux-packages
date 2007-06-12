a90990fdbf88f12d39af4b714c5dac625c61d9bf
krichy <krichy@cb50d35e-b264-da11-8148-0002b3c91c9d>
2007-06-12 07:42:28 +0000

    Added an endless loop around dnscache.
    git-svn-id: svn+ssh://git.cflinux.hu/svn/cflinux/packages@533 cb50d35e-b264-da11-8148-0002b3c91c9d

--- djbdns/rc.d
+++ djbdns/rc.d
@@ -17,11 +17,13 @@ dnscache_seed="/usr/local/etc/dnscache/seed"
 case "$dnscache_enable" in
 	[yY][eE][sS])
 		echo -n 'Starting domain name cache server:'
-		env ROOT=$dnscache_root CACHESIZE=$dnscache_cachesize \
-			IP=$dnscache_ip IPSEND=$dnscache_ipsend \
-			UID=$(id -u $dnscache_user) GID=$(id -g $dnscache_user) \
-			/usr/local/bin/dnscache \
-			< $dnscache_seed > /dev/null 2>&1 &
+		while sleep 1 ; do
+			env ROOT=$dnscache_root CACHESIZE=$dnscache_cachesize \
+				IP=$dnscache_ip IPSEND=$dnscache_ipsend \
+				UID=$(id -u $dnscache_user) GID=$(id -g $dnscache_user) \
+				/usr/local/bin/dnscache \
+				< $dnscache_seed > /dev/null 2>&1
+		done < /dev/null > /dev/null 2>&1 &
 		echo ' dnscache'
 	;;
 esac
